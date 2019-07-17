53084ae8ceb040fab239bf7099e84652c608fcaf
kmocorro <kevinmocorro.github@gmail.com>
2019-07-17 16:04:22 +0800

    a

--- index.js
+++ index.js
@@ -12,12 +12,16 @@ module.exports = async (req, res) => {
         from: "Kartero <f4automailer@sunpowercorp.com>",
         to: js.recipient_email,
         subject: js.subject,
-        text: 'I am your new Mailman, Meet Kartero.'
+        text: 'I am your new Mailman, Meet Kartero.',
+        attachments: {
+            filename: js.filename,
+            path: './static/' + js.filename
+        }
     }
 
     transporter.sendMail(mailSetup, (err, info) => {
         if(err){ return err; }
-        
+
         console.log(info);
         res.end('Successfully sent.');
     })
