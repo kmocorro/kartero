dbe7e2a62fe4275a998aa73dba36b5efac930229
kmocorro <kevinmocorro.github@gmail.com>
2019-07-18 08:19:46 +0800

    w

--- index.js
+++ index.js
@@ -5,24 +5,19 @@ const config = require('./config').config;
 module.exports = async (req, res) => {
     const js = await (json(req));
     const transporter = nodemailer.createTransport(config.mail);
+    const filePath = './static/' + js.filename 
 
-    console.log(js);
-
+    /** In order to attach file in email, ssh through the server, move the file you want to attach to "~App/kartero/static/" and use its filename in curl or POST request. */
     const mailSetup = {
-        from:  js.sender + " <f4automailer@sunpowercorp.com>",
+        from:  js.sender,
         to: js.recipient_email,
         subject: js.subject,
         text: js.message,
-        attachments: {
-            filename: js.filename,
-            path: './static/' + js.filename
-        }
+        attachments: { filename: js.filename, path: filePath }
     }
-
+    
     transporter.sendMail(mailSetup, (err, info) => {
-        if(err){ return err; }
-
-        console.log(info);
-        res.end('Successfully sent.');
+        if(err){ return res.end(err + ' error sending email.') }
+        res.end('Successfully sent to ' + info.accepted);
     })
 }
