e7256f8eda32a590e90432aef2f43f78779e111f
kmocorro <kevinmocorro.github@gmail.com>
2020-07-17 11:31:55 +0800

    aaa

--- index.js
+++ index.js
@@ -5,19 +5,38 @@ const config = require('./config').config;
 module.exports = async (req, res) => {
     const js = await (json(req));
     const transporter = nodemailer.createTransport(config.mail);
-    const filePath = './static/' + js.filename || "",
+    const filePath = './static/' + js.filename 
 
-    /** In order to attach file in email, ssh through the server, move the file you want to attach to "~App/kartero/static/" and use its filename in curl or POST request. */
-    const mailSetup = {
-        from:  js.sender + ' <f4automailer@sunpowercorp.com>',
-        to: js.recipient_email,
-        subject: js.subject,
-        text: js.message,
-        attachments: { filename: js.filename, path: filePath }
+    if(!filePath){
+
+        /** In order to attach file in email, ssh through the server, move the file you want to attach to "~App/kartero/static/" and use its filename in curl or POST request. */
+        const mailSetup = {
+            from:  js.sender + ' <f4automailer@sunpowercorp.com>',
+            to: js.recipient_email,
+            subject: js.subject,
+            text: js.message,
+        }
+
+        transporter.sendMail(mailSetup, (err, info) => {
+            if(err){ return res.end(err + ' error sending email.') }
+            res.end('Successfully sent to ' + info.accepted);
+        })
+    
+
+    } else {
+
+        /** In order to attach file in email, ssh through the server, move the file you want to attach to "~App/kartero/static/" and use its filename in curl or POST request. */
+        const mailSetup = {
+            from:  js.sender + ' <f4automailer@sunpowercorp.com>',
+            to: js.recipient_email,
+            subject: js.subject,
+            text: js.message,
+            attachments: { filename: js.filename, path: filePath }
+        }
+
+        transporter.sendMail(mailSetup, (err, info) => {
+            if(err){ return res.end(err + ' error sending email.') }
+            res.end('Successfully sent to ' + info.accepted);
+        })
     }
-
-    transporter.sendMail(mailSetup, (err, info) => {
-        if(err){ return res.end(err + ' error sending email.') }
-        res.end('Successfully sent to ' + info.accepted);
-    })
 }
